22298ba5f0e7bd3db6f287cc2aaa3405da69a9b2
fqjun <1767851382@qq.com>
2023-06-25 17:17:07 +0800

    feature:add msg file for sub and pub

--- src/plumbing_pub_sub/src/demo02_sub.cpp
+++ src/plumbing_pub_sub/src/demo02_sub.cpp
@@ -1,45 +1,29 @@
 /*
-    需求: 实现基本的话题通信，一方发布数据，一方接收数据，
-         实现的关键点:
-         1.发送方
-         2.接收方
-         3.数据(此处为普通文本)
-
-
-    消息订阅方:
-        订阅话题并打印接收到的消息
-
-    实现流程:
-        1.包含头文件 
-        2.初始化 ROS 节点:命名(唯一)
-        3.实例化 ROS 句柄
-        4.实例化 订阅者 对象
-        5.处理订阅的消息(回调函数)
-        6.设置循环调用回调函数
+    需求: 订阅人的信息
 
 */
-// 1.包含头文件 
-#include "ros/ros.h"
-#include "std_msgs/String.h"
 
-void doMsg(const std_msgs::String::ConstPtr& msg_p){
-    ROS_INFO("我听见:%s",msg_p->data.c_str());
-    // ROS_INFO("我听见:%s",(*msg_p).data.c_str());
+#include "ros/ros.h"
+#include "plumbing_pub_sub/Personal.h"
+
+void doPerson(const plumbing_pub_sub::Personal::ConstPtr& person_p){
+    ROS_INFO("订阅的人信息:%s, %d, %.2f", person_p->name.c_str(), person_p->age, person_p->height);
 }
-int main(int argc, char  *argv[])
+
+int main(int argc, char *argv[])
 {
     setlocale(LC_ALL,"");
-    //2.初始化 ROS 节点:命名(唯一)
-    ros::init(argc,argv,"listener");
-    //3.实例化 ROS 句柄
+
+    //1.初始化 ROS 节点
+    ros::init(argc,argv,"listener_person");
+    //2.创建 ROS 句柄
     ros::NodeHandle nh;
+    //3.创建订阅对象
+    ros::Subscriber sub = nh.subscribe<plumbing_pub_sub::Personal>("chatter_person",10,doPerson);
 
-    //4.实例化 订阅者 对象
-    ros::Subscriber sub = nh.subscribe<std_msgs::String>("chatter",10,doMsg);
-    //5.处理订阅的消息(回调函数)
+    //4.回调函数中处理 person
 
-    //     6.设置循环调用回调函数
-    ros::spin();//循环读取接收的数据，并调用回调函数处理
-
+    //5.ros::spin();
+    ros::spin();
     return 0;
 }
